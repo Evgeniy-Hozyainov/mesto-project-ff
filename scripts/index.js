4e708d5ac198b9cb87f79857750c54b75bc275f8
Evgeniy-Hozyainov <c0r2air@yandex.ru>
2024-01-18 11:00:14 +0300

    Начальный проект

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.getElementById('card-template').content
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list')
 
 // @todo: Функция создания карточки
+function createCardDOM({ name, link }, fnDeleteCard) {
+    const card = cardTemplate.querySelector('.card').cloneNode(true)
+    card.querySelector('.card__image').src = link
+    card.querySelector('.card__title').textContent = name
+    card.querySelector('.card__delete-button').addEventListener('click', fnDeleteCard)
+
+    return card
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(evt) {
+    evt.target.closest('.card').remove();
+}
 
 // @todo: Вывести карточки на страницу
+function showInitialCards() {
+    const cardsDOM = initialCards.map(cardData => {
+        return createCardDOM(cardData, deleteCard)
+    })
+
+    placesList.append(...cardsDOM)
+}
+
+showInitialCards()
